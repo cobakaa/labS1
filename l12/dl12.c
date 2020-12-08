fac1a235daba2ca3f091465c0e7dd6bfac9ca51c
cobakaa <kyastakhov@gmail.com>
2020-12-08 19:05:42 +0300

    added 2nd var for neg numbs

--- l12/dl12.c
+++ l12/dl12.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
-void bin(unsigned n, unsigned l, unsigned y) // a, длина, число 0
+void bin(unsigned n, int l, int y)
 {
     unsigned i;
     int x;
-
     for (i = 1 << (l - 1); i > 0; i = i / 2)
     {
         x = y;
-        //printf("x = %d \n", x);
+
         if (i == 1 << (l / 2))
         {
             while (x != 0)
             {
                 x = x - 1;
-               // printf("x = %d \n", x);
                 printf("0");
             }
         }
         (n & i) ? printf("1") : printf("0");
     }
+    printf("\n");
 }
 
 int main(void)
 {
-    int a, a1, a2, kd, kd1, k, k1, d;
+    int a, a1, a2, kd, k, k1, d;
 
-    scanf("%d", &a);
-    if ((a != ' ') && (a != EOF))
+    while (scanf("%d", &a) == 1)
     {
+        if (a == 0)
+        {
+            printf("0000000000");
+        }
         if (a > 0)
         {
             k = 0;
@@ -46,10 +48,30 @@ int main(void)
                 d = d * 2;
                 kd = kd + 1;
             }
-            kd1 = kd / 2;
-
             bin(a2, kd, k1);
-            printf("\n");
+        }
+
+        if (a < 0)
+        {
+            a = -a;
+            k = 0;
+            a1 = a;
+            a2 = a;
+            while (a != 0)
+            {
+                a = a / 10;
+                k = k + 1;
+            }
+            k1 = 10 - k;
+            d = 1;
+            kd = 0;
+            while (a1 >= d)
+            {
+                d = d * 2;
+                kd = kd + 1;
+            }
+            printf("-");
+            bin(a2, kd, k1);
         }
     }
 }
